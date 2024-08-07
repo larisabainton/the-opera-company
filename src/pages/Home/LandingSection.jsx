1e15590b6d7d764936bea552555ef540e21bdaee
Larisa Bainton <larisabainton@gmail.com>
2024-08-07 19:06:46 -0400

    style landing page

--- src/pages/Home/LandingSection.jsx
+++ src/pages/Home/LandingSection.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import TicketsButton from "../../components/TicketsButton"
+import BackgroundVideo from "../../assets/backgroundVideo.mp4";
 
 const LandingSection = () => {
     return (
         <div className="LandingSection">
-            <div className="LandingSection_background background"></div>
+            <div className="LandingSection_background background">
+                <video autoPlay loop muted playsInline className="background-video">
+                    <source src={BackgroundVideo} type="video/mp4"/>
+                </video>
+            </div>
             <div className="LandingSection_foreground foreground">
                 <div className="foreground_hook">Welcome to the Opera</div>
                 <TicketsButton />
