bb181af4b7ff65c45f53cd83dca1c8dc8a464d4f
Larisa Bainton <larisabainton@gmail.com>
2025-01-04 12:01:43 -0500

    wip

--- src/pages/Home/LandingSection.jsx
+++ src/pages/Home/LandingSection.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import TicketsButton from "../../components/TicketsButton"
-import BackgroundVideo from "../../assets/backgroundVideo.mp4";
+import { StaticImage } from "gatsby-plugin-image";
+// import BackgroundVideo from "../../assets/backgroundVideo.mp4";
 
 const LandingSection = () => {
     return (
         <div className="LandingSection">
             <div className="LandingSection_background background">
-                <video autoPlay loop muted playsInline className="background-video">
+                <StaticImage alt="" className="" src="../../assets/background.png" placeholder="none" />
+                {/* <video autoPlay loop muted playsInline className="background-video">
                     <source src={BackgroundVideo} type="video/mp4"/>
-                </video>
+                </video> */}
             </div>
             <div className="LandingSection_foreground foreground">
                 <div className="foreground_hook">Welcome to the Opera</div>
