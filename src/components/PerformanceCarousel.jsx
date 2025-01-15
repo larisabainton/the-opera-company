8b87b3b6978e4dc54905e6a3de778f0912186dce
Larisa Bainton <larisabainton@gmail.com>
2025-01-14 22:10:19 -0500

    get rid of extra sections

--- src/components/PerformanceCarousel.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const PerformanceCarousel = () => {
-    return (
-        <div className="PerformanceCarousel"></div>
-    )
-};
-
-export default PerformanceCarousel;
--- src/components/QuestionSection.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const QuestionSection = () => {
-    return (
-        <div className="QuestionSection"></div>
-    )
-};
-
-export default QuestionSection;
